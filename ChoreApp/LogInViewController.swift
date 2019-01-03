2d2d52151aaa4a40c3019dad530aca3c0857f17f
Isaac Hu (student LM) <s021791@lm-19s021791.lmsd.org>
2019-01-03 10:32:42 -0500

    Created the basics of the 3 account/login screens

---
+++ ChoreApp/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  ChoreApp
+//
+//  Created by Isaac Hu (student LM) on 1/3/19.
+//  Copyright © 2019 SeminarGroup. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChoreApp/StartingScreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartingScreenViewController.swift
+//  ChoreApp
+//
+//  Created by Isaac Hu (student LM) on 1/3/19.
+//  Copyright © 2019 SeminarGroup. All rights reserved.
+//
+
+import UIKit
+
+class StartingScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
