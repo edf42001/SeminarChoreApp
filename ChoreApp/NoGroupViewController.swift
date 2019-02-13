fa2c01517ec0cd07bb52319c5950de2380925212
Ethan Frank <s011291@students.lmsd.org>
2019-02-13 10:09:54 -0500

    Remove thing in no group view controller

--- ChoreApp/NoGroupViewController.swift
+++ ChoreApp/NoGroupViewController.swift
@@ -28,53 +28,33 @@ class NoGroupViewController: UIViewController {
         createGroupButton.applyButtonStyles(type: .standard)
         
         setupEnterGroupNameAlert()
-    
-        if Auth.auth().currentUser==nil {
-            let email = "ethan@me.com"
-            let password = "passwordpassword"
-            Auth.auth().createUser(withEmail: email, password: password, completion: nil)
-        }
-        
-        //FOR TESTING PURPOSES Setup user and group objects for next controller
-        if let uid = Auth.auth().currentUser?.uid {
-            user = User(uid: uid, username: "bobisthebest", email: "ethan@me.com", isParent: true)
-//            user?.groupID = "-LVE5XGJ5ZNvT-ZnnJZ0"
-//            let emptyUserArray:[UserInfo] = []
-            
-//            group = Group(id: "-LVE5XGJ5ZNvT-ZnnJZ0", name: "Groooup", parents: emptyUserArray, children: emptyUserArray, chores: nil)
-            DatabaseHandler.observeIfAddedToGroup(uid: uid, onRecieve: {groupID in
-                if let groupID = groupID as? String {
-                    DatabaseHandler.readBasicGroupData(groupID: groupID, uid: self.user!.uid, completion: {group, isParent in
-                        self.group = group
-                        if isParent {
-                            print("User is parent")
-                            self.user?.isParent = true
-                            self.toScreen = 1
-                            DatabaseHandler.getAllChoresFromGroup(groupID: groupID, completion: {chores in
-                                self.group?.chores = chores
-                                self.performSegue(withIdentifier: "toParent", sender: self)
-                            })
-                            
-                        }else {
-                            print("User is child")
-                            self.user?.isParent = false
-                            self.toScreen = 2
-                            DatabaseHandler.getChoresForUser(uid: self.user!.uid, groupID: groupID, completion: {chores in
-                                self.user?.chores = chores
-                                self.performSegue(withIdentifier: "toChild", sender: self)
-                            })
-                        }
-                        DatabaseHandler.stopObservingIfAddedToGroup()
-                    })
-                }
-            })
-        //End for testing purposes
-    }
-    }
-    
-    //Also only for testing purposes
-    override func viewDidAppear(_ animated: Bool) {
-        
+
+        DatabaseHandler.observeIfAddedToGroup(uid: user!.uid, onRecieve: {groupID in
+            if let groupID = groupID as? String {
+                DatabaseHandler.readBasicGroupData(groupID: groupID, uid: self.user!.uid, completion: {group, isParent in
+                    self.group = group
+                    if isParent {
+                        print("User is parent")
+                        self.user?.isParent = true
+                        self.toScreen = 1
+                        DatabaseHandler.getAllChoresFromGroup(groupID: groupID, completion: {chores in
+                            self.group?.chores = chores
+                            self.performSegue(withIdentifier: "toParent", sender: self)
+                        })
+                        
+                    }else {
+                        print("User is child")
+                        self.user?.isParent = false
+                        self.toScreen = 2
+                        DatabaseHandler.getChoresForUser(uid: self.user!.uid, groupID: groupID, completion: {chores in
+                            self.user?.chores = chores
+                            self.performSegue(withIdentifier: "toChild", sender: self)
+                        })
+                    }
+                    DatabaseHandler.stopObservingIfAddedToGroup()
+                })
+            }
+        })
     }
     
     @IBAction func openSettings(_ sender: UIButton) {
@@ -98,6 +78,7 @@ class NoGroupViewController: UIViewController {
     
     func createNewGroup(name: String){
         if let uid = user?.uid {
+            DatabaseHandler.stopObservingIfAddedToGroup()
             DatabaseHandler.createGroup(uid: uid, name: name) {key in
                 guard let user = self.user else {return}
                 user.groupID = key
