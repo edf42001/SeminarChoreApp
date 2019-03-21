93344be8ee735cc013e03de4e5a659248709f913
Isaac Hu (student LM) <s021791@lm-19s021791.lmsd.org>
2019-03-21 10:35:21 -0400

    Broke something or another gl hf

--- ChoreApp/Controllers/CustomTabBarController.swift
+++ ChoreApp/Controllers/CustomTabBarController.swift
@@ -8,20 +8,40 @@
 
 import UIKit
 
-class CustomTabBarController: UITabBarController {
+class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
     var user:User?
     var group:Group?
     
+    var yGroup:UIViewController?
+    var nGroup:UIViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.delegate = self
+        
         if group == nil {
             self.selectedIndex = 1
         }else{
             self.selectedIndex = 0
         }
         
-        self.selectedIndex = 2 //for testing only
+        let board = UIStoryboard(name: "Group", bundle: nil)
+        yGroup = board.instantiateViewController(withIdentifier: "YesGroupViewController")
+        nGroup = board.instantiateViewController(withIdentifier: "NoGroupViewController")
+        
+        //self.selectedIndex = 2 ////for testing only
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print(item.title)
+        if item.title == "Group" {
+            print(self.viewControllers?.count)
+            if var controllers = self.viewControllers {
+                controllers[1] = (group == nil ? nGroup! : yGroup!)
+            }
+            self.viewControllers![1] = (group == nil ? nGroup! : yGroup!)
+        }
+       
     }
     
 
